3750238c68d64c5edc538c64831ab53af012fc7f
andrewconstancio <61609406+andrewconstancio@users.noreply.github.com>
2023-06-06 22:31:10 -0500

    hello5

--- url_shortner_api/settings.py
+++ url_shortner_api/settings.py
@@ -78,6 +78,10 @@ WSGI_APPLICATION = 'url_shortner_api.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config()
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -117,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
